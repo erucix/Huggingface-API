5dc0651c216d6642f49eb33e463e72e2ff273b1d
erucix <erucix@gmail.com>
2023-07-24 21:49:33 +0530

    modified:   hug-1.js 	modified:   hug.js

--- hug-1.js
+++ hug-1.js
@@ -12,9 +12,61 @@ const hfchat = process.env.HFCHAT
 const cookie = `token=${token}; hf-chat=${hfchat}`
 
 function hug(msg) {
-    return new Promise((resolve) => {
+    return new Promise(async (resolve) => {
         if (!token) return "Huggingface token not provided"
         if (!hfchat) return "hfchat not provided"
+
+        let data = process.env.CONV ? { "conversationId": process.env.CONV } : await newConversationId().then(data => data)
+
+        process.env.CONV = data.conversationId
+        fetch(`https://huggingface.co/chat/conversation/${data.conversationId}/__data.json?x-sveltekit-invalidated=1_1`, {
+            "headers": {
+                "cookie": cookie
+            },
+            "body": null,
+            "method": "GET"
+        })
+            .then(() => {
+                fetch(`https://huggingface.co/chat/conversation/${data.conversationId}`, {
+                    "headers": {
+                        "content-type": "application/json",
+                        "cookie": cookie
+                    },
+                    "body": JSON.stringify({
+                        "inputs": msg,
+                        "parameters": {
+                            "temperature": 0.2,
+                            "truncate": 1000,
+                            "max_new_tokens": 1024,
+                            "stop": [
+                                "</s>"
+                            ],
+                            "top_p": 0.95,
+                            "repetition_penalty": 1.2,
+                            "top_k": 50,
+                            "return_full_text": false
+                        },
+                        "stream": true,
+                        "options": {
+                            "id": randomUUID(),
+                            "response_id": randomUUID(),
+                            "is_retry": false,
+                            "use_cache": false,
+                            "web_search_id": ""
+                        }
+                    }),
+                    "method": "POST"
+                }).then(dataa => dataa.text())
+                    .then(dataa => {
+                        dataa = JSON.parse(dataa.slice(dataa.lastIndexOf('data:{"token":{') + 'data:'.length)).generated_text
+                        resolve(dataa)
+                    })
+            })
+
+    })
+}
+function newConversationId() {
+    return new Promise((resolve) => {
         fetch("https://huggingface.co/chat/conversation", {
             "headers": {
                 "content-type": "application/json",
@@ -22,62 +74,14 @@ function hug(msg) {
             },
             "body": "{\"model\":\"OpenAssistant/oasst-sft-6-llama-30b-xor\"}",
             "method": "POST"
-        })
-            .then(data => data.json())
-            .then(data => {
-                fetch(`https://huggingface.co/chat/conversation/${data.conversationId}/__data.json?x-sveltekit-invalidated=1_1`, {
-                    "headers": {
-                        "cookie": cookie
-                    },
-                    "body": null,
-                    "method": "GET"
-                })
-                    .then(() => {
-                        fetch(`https://huggingface.co/chat/conversation/${data.conversationId}`, {
-                            "headers": {
-                                "content-type": "application/json",
-                                "cookie": cookie
-                            },
-                            "body": JSON.stringify({
-                                "inputs": msg,
-                                "parameters": {
-                                    "temperature": 0.2,
-                                    "truncate": 1000,
-                                    "max_new_tokens": 1024,
-                                    "stop": [
-                                        "</s>"
-                                    ],
-                                    "top_p": 0.95,
-                                    "repetition_penalty": 1.2,
-                                    "top_k": 50,
-                                    "return_full_text": false
-                                },
-                                "stream": true,
-                                "options": {
-                                    "id": randomUUID(),
-                                    "response_id": randomUUID(),
-                                    "is_retry": false,
-                                    "use_cache": false,
-                                    "web_search_id": ""
-                                }
-                            }),
-                            "method": "POST"
-                        }).then(dataa => dataa.text())
-                            .then(dataa => {
-                                dataa = JSON.parse(dataa.slice(dataa.lastIndexOf('data:{"token":{') + 'data:'.length)).generated_text
-                                resolve(dataa)
-                            })
-                    })
-
-
-            })
+        }).then(data => data.json())
+            .then(data => resolve(data))
     })
 }
-
 function ask() {
-    rl.question("\033[35mYou: \033[34m> \033[36m", (msg) => {
+    rl.question("You: > ", (msg) => {
         hug(msg).then(data => {
-            console.log("😊 : \033[34m" + data)
+            console.log("😊 : " + data)
             ask()
         })
     })
